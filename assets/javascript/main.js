7d452669d9ba0c2e8cabe0a915b80b1769274d86
devingprice <devingprice@gmail.com>
2019-04-15 12:15:23 -0400

    Add function to get first review

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,29 @@
 // 8dOqvd6QOBI7Uy45Sp5I2cSmlYZQva13
 
 //https://api-us.faceplusplus.com/facepp/v3/detect?api_key=8dOqvd6QOBI7Uy45Sp5I2cSmlYZQva13
+
+
+function getFirstReview( movieName ){
+    var urlBase = 'https://api.themoviedb.org/3/search/movie?';
+    var apiKey = 'api_key=7c49e1342952d7c7e126e900862f9e64';
+    var movieSearch = urlBase + apiKey + '&query=' + movieName
+    $.ajax({
+        url: movieSearch,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        var firstRes = response.results[0];
+
+        var reviewSearch = "https://api.themoviedb.org/3/movie/" + firstRes.id + "/reviews?"
+        reviewSearch += apiKey;
+        $.ajax({
+            url: reviewSearch,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+
+        })
+    })
+}
+
+getFirstReview( 'Frozen' );
